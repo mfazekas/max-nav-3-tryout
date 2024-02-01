90aee88a5af9522479af353532876ff348f44d81
Miklós Fazekas <mfazekas@szemafor.com>
2024-02-01 09:16:39 +0100

    fix: RECEIVER_EXPORTED / RECEIVER_NOT_EXPORTED

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -10,7 +10,7 @@ android {
     defaultConfig {
         applicationId = "com.example.navigation3tryout"
         minSdk = 29
-        targetSdk = 34
+        targetSdk = 33
         versionCode = 1
         versionName = "1.0"
 
